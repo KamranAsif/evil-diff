23519a9b32b42d8d1000b1cd448382e6a7c9471b
Kamran Asif <me@kamranasif.com>
2017-10-05 08:44:43 -0400

    Adding comments and cleanup. More tests

--- src/walkTree.ts
+++ src/walkTree.ts
@@ -2,15 +2,22 @@ import { isObject } from './isObject';
 import { shallowClone } from './shallowClone';
 
 /**
- * Recursively walks tree and clones paths that have changed
+ * Recursively walks tree, copy values to revision object and cloning
+ * paths that have cloned.
  */
 export const walkTree = <T>(source: T, revision: T): T => {
-  // Return early for no op
+  // Return early for no op.
   if (source === revision) {
     return source;
   }
 
-  // Check if both objects are the same. This catches Arr/Obj merging
+  // Return revision if its a non object type.
+  // This also catches null/undefined values.
+  if (!isObject(source) || !isObject(revision)) {
+    return revision;
+  }
+
+  // Check if both objects are the same. This catches Arr/Obj merging.
   const matchingType = source instanceof revision.constructor &&
                        revision instanceof source.constructor;
 
@@ -18,56 +25,62 @@ export const walkTree = <T>(source: T, revision: T): T => {
     return revision;
   }
 
-  // Return revision if its a non object type
-  if (!isObject(source) || !isObject(revision)) {
-    return revision;
-  }
-
-  // Gather keys. For arrays, its just a list of indexes
-  // TODO (asif) See if it'll be quicker walk arrays instead of getting its keys
+  // Gather keys. For arrays, its just a list of indexes.
+  // TODO(asif): Test walking arrays instead of getting its keys.
   type keyType = keyof T;
   const sourceKeys = Object.keys(source) as keyType[];
   const revisionKeys = Object.keys(revision) as keyType[];
 
-  let changed = false;
-  let deleted = 0;
+  let cloned = false;
+  let deletedCount = 0;
 
   for (const key of sourceKeys) {
     const sourceValue = source[key];
     const revisionValue = revision[key];
+
+    // Recursive call. If newValue is deeply equal to sourceValue, then 
+    // we return sourceValue, allowing us to skip cloning.
     const newValue = walkTree(sourceValue, revisionValue);
 
-    // If walkTree doesn't return new object, that means no change
+    // If walkTree doesn't return new object, that means no change.
     if (newValue === sourceValue) {
       continue;
     }
 
     // On first change, clone current object.
-    // This needs to be done before we set the newValue to the property
-    // TODO(asif): Remove isObject call when TS/pull/13288 is merged in
-    if (!changed && isObject(source)) {
+    // This needs to be done before we set the newValue to the property, so 
+    // we don't modify the original object.
+    // TODO(asif): Remove isObject call when TS/pull/13288 is merged in.
+    if (!cloned && isObject(source)) {
       source = shallowClone(source);
-      changed = true;
+      cloned = true;
     }
 
     if (newValue === undefined) {
-      deleted++;
+      deletedCount++;
+      // TODO(asif): Investigate performance of delete vs setting undefined. 
+      // TODO(asif): Add configuration property to prefer undefined vs delete.
+      delete source[key];
+    } else {
+      source[key] = newValue;
     }
-
-    source[key] = newValue;
   }
 
-  if (sourceKeys.length === revisionKeys.length - deleted) {
+  // Check if there are any new keys in revision object.
+  if (sourceKeys.length + deletedCount === revisionKeys.length) {
     return source;
   }
 
-  // TODO(asif): Remove isObject call when TS/pull/13288 is merged in
-  if (!changed && isObject(source)) {
+  // There are new keys, so we should clone if we haven't done so already.
+  // TODO(asif): Remove isObject call when TS/pull/13288 is merged in.
+  if (!cloned && isObject(source)) {
     source = shallowClone(source);
-    changed = true;
+    cloned = true;
   }
 
+  // TODO(asif): Optimize this best O(n) to worst O(n) by switching to a set.
   for (const key of revisionKeys) {
+    //TODO(asif): Use deletedCount to return early.
     const sourceHasKey = key in source;
     if (!sourceHasKey) {
       source[key] = revision[key];
--- test/revise-spec.ts
+++ test/revise-spec.ts
@@ -2,8 +2,8 @@ import {revise} from '../src/revise';
 import { assert } from 'chai';
 
 describe('revise', () => {
-  describe('works with primitives', () => {
-    it('should return unchanged value', () => {
+  describe('given primitives', () => {
+    it('returns unchanged value', () => {
       const sources = [1, 'two', false, null, undefined];
       const revisions = [1, 'two', false, null, undefined];
       for (let i = 0; i < sources.length; i++) {
@@ -13,83 +13,111 @@ describe('revise', () => {
         assert.strictEqual(newSource, source, 'Should return same value');
       }
     });
-    it('should return changed values', () => {
+    it('returns changed values', () => {
       const sources = [1, 'two', false, null, undefined];
       const revisions = [2, 'three', true, '4', 5];
       for (let i = 0; i < sources.length; i++) {
         const source = sources[i];
         const revision = revisions[i];
         const newSource = revise(source, revision);
+        const newRevision = revise(revision, source);
         assert.strictEqual(newSource, revision, 'Should return new value');
+        assert.strictEqual(newRevision, source, 'Should return new value');
       }
     });
   });
 
-  describe('works with objects', () => {
-    it('should return unchanged object if no change', () => {
+  describe('given objects', () => {
+    it('returns unchanged object if no change', () => {
       const source = {foo: 1};
       const revision = {foo: 1};
       const newSource = revise(source, revision);
       assert.strictEqual(newSource, source, 'Should return same obj');
     });
-    it('should return new object if changed', () => {
+    it('returns new object if changed', () => {
       const source = {foo: 1};
       const revision = {foo: 2};
       const newSource = revise(source, revision);
       assert.notStrictEqual(newSource, source, 'Should return a new obj');
     });
-    it('should take values from revision object', () => {
+    it('takes values from revision object', () => {
       const source = {foo: 1};
       const revision = {foo: 2};
       const newSource = revise(source, revision);
-      assert.deepEqual(newSource, revision, 'Should have values from second obj');
+      assert.deepEqual(newSource, revision,
+          'Should have values from second obj');
     });
-    it('should take values from revision object', () => {
+    it('takes new values from revision object', () => {
       const source = {foo: 1};
       const revision = {foo: 1, bar: 2};
       const newSource = revise(source, revision);
-      assert.deepEqual(newSource, revision, 'Should have values from second obj');
+      assert.deepEqual(newSource, revision,
+          'Should have new values from second obj');
+    });
+    it('removes deleted values from source object', () => {
+      const source = {foo: 1, bar: 2};
+      const revision = {foo: 1};
+      const newSource = revise(source, revision);
+      assert.deepEqual(newSource, revision,
+          'Should have new values from second obj');
     });
   });
 
-  describe('works with arrays', () => {
-    it('should return unchaged array if no change', () => {
+  describe('given arrays', () => {
+    it('returns unchaged array if no change', () => {
       const source = [1, 2, 3];
       const revision = [1, 2, 3];
       const newSource = revise(source, revision);
       assert.strictEqual(newSource, source, 'Should return same obj');
     });
-    it('should return new array if changed', () => {
+    it('returns new array if changed', () => {
       const source = [1, 2, 3];
       const revision = [3, 2, 1];
       const newSource = revise(source, revision);
       assert.notStrictEqual(newSource, source, 'Should return a new obj');
     });
-    it('should take values from revision array', () => {
+    it('takes values from revision array', () => {
       const source = [1, 2, 3];
       const revision = [3, 2, 1];
       const newSource = revise(source, revision);
-      assert.deepEqual(newSource, revision, 'Should have values from second obj');
+      assert.deepEqual(newSource, revision,
+          'Should have values from second obj');
+    });
+    it('takes new values from revision array', () => {
+      const source = [1, 2, 3];
+      const revision = [1, 2, 3, 4];
+      const newSource = revise(source, revision);
+      assert.deepEqual(newSource, revision,
+          'Should have values from second obj');
+    });
+    it('removes deleted values from source array', () => {
+      const source = [1, 2, 3, 4];
+      const revision = [1, 2, 3];
+      const newSource = revise(source, revision);
+      assert.deepEqual(newSource, revision,
+          'Should have values from second obj');
     });
   });
 
-  describe('works with functions', () => {
-    it('should return unchaged function if no change', () => {
+  describe('given functions', () => {
+    it('returns unchaged function if no change', () => {
       const source = () => {};
       const revision = source;
       const newSource = revise(source, revision);
-      assert.strictEqual(newSource, source, 'Should return original function');
+      assert.strictEqual(newSource, source,
+          'Should return original function');
     });
-    it('should return new function if changed', () => {
+    it('returns new function if changed', () => {
       const source = (a: string) => {};
       const revision = (b: string) => {};
       const newSource = revise(source, revision);
-      assert.strictEqual(newSource, revision, 'Should return new function');
+      assert.strictEqual(newSource, revision,
+          'Should return new function');
     });
   });
 
-  describe('works with mixed types', () => {
-    it('Shouldn\'t merge array and object', () => {
+  describe('given mixed types', () => {
+    it(`doesn't merge arrays and objects`, () => {
       const arr: any = ['a', 'b', 'c'];
       const obj: any = { 0: 'a', 1: 'b', 2: 'c' };
       const objRevision = revise(arr, obj);
@@ -99,20 +127,23 @@ describe('revise', () => {
     });
   });
 
-  it('should have values from revision', () => {
+  it('returns values from revision', () => {
     const source = {foo: {bar: 1}, baz: [1, 2, 3]};
     const revision1 = {foo: {bar: 2}, baz: [1, 2, 3]};
     const revision2 = {foo: {bar: 1}, baz: [3, 2, 1]};
 
-    //debugger;
     const diff2Obj = revise(source, revision1);
     const diff3Obj = revise(source, revision2);
 
-    assert.strictEqual(diff2Obj.baz, source.baz, 'Should keep object path unmodified path');
-    assert.notStrictEqual(diff2Obj.foo, source.foo, 'Should return new path for changed value');
+    assert.strictEqual(diff2Obj.baz, source.baz,
+      'Should keep object path unmodified path');
+    assert.notStrictEqual(diff2Obj.foo, source.foo,
+      'Should return new path for changed value');
 
-    assert.strictEqual(diff3Obj.foo, source.foo, 'Should keep array path unmodified path');
-    assert.notStrictEqual(diff3Obj.baz, source.baz, 'Should return new path for changed value');
+    assert.strictEqual(diff3Obj.foo, source.foo,
+      'Should keep array path unmodified path');
+    assert.notStrictEqual(diff3Obj.baz, source.baz,
+      'Should return new path for changed value');
   });
- 
+
 });
