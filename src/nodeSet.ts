d230c48eca61e36507a588586574bcc0fd310e83
Kamran Asif <me@kamranasif.com>
2017-11-04 12:42:25 -0400

    Fixing collisions in node set

--- src/nodeSet.ts
+++ src/nodeSet.ts
@@ -1,42 +1,38 @@
-export const NODE_SYMBOL = Symbol();
-
+export const testSymbol = Symbol();
 export const node1 = {
-  [NODE_SYMBOL]: 1,
+  [testSymbol]: true,
 };
-
 export const node2 = {
-  [NODE_SYMBOL]: undefined,
+  [testSymbol]: undefined,
 };
 
 // TODO(asif): Get correct typings once TS/issue/1863 is resolved.
 export type Node = typeof node1|typeof node2;
-
-let COUNTER = 0;
 
 /**
  * Replacement for WeakSet that checks if a node exists in a set.
  * Optimized for memory usage and performance.
  */
 export class NodeSet<T extends Node> {
-  /** Index used to avoid NodeSet collisions. */
-  private idx: number;
+  /** Symbol used to avoid set collisions. */
+  private setSymbol: symbol;
 
   constructor() {
-    this.idx = COUNTER++;
+    this.setSymbol = Symbol();
   }
 
   /** Removes node to set. */
   public add(node: T) {
-    node[NODE_SYMBOL] = this.idx;
+    node[this.setSymbol] = true;
   }
 
   /** Check if node in set. */
   public has(node: T): boolean {
-    return node[NODE_SYMBOL] === this.idx;
+    return node[this.setSymbol] === true;
   }
 
   /** Removes node from set. */
   public remove(node: T) {
-    delete node[NODE_SYMBOL];
+    delete node[this.setSymbol];
   }
 }
--- test/nodeSet-spec.ts
+++ test/nodeSet-spec.ts
@@ -43,4 +43,34 @@ describe('NodeSet', () => {
       assert.isFalse(nodeSet.has(foo));
     });
   });
+
+  describe('when there are multiple nodeSets', () => {
+    let foo: {};
+    let nodeSet2: NodeSet<{}>;
+
+    beforeEach(() => {
+      nodeSet2 = new NodeSet();
+
+      foo = [1, 2, 3];
+      nodeSet.add(foo);
+    });
+
+    it('should not leak to other sets', () => {
+      assert.isTrue(nodeSet.has(foo));
+      assert.isFalse(nodeSet2.has(foo));
+    });
+
+    it('should not collide', () => {
+      nodeSet2.add(foo);
+      assert.isTrue(nodeSet.has(foo));
+      assert.isTrue(nodeSet2.has(foo));
+    });
+
+    it('Should only be removed from one node set', () => {
+      nodeSet2.add(foo);
+      nodeSet.remove(foo);
+      assert.isFalse(nodeSet.has(foo));
+      assert.isTrue(nodeSet2.has(foo));
+    });
+  });
 });
